99419e0445369ad0e0faae87b77d1fb81c810129
Alexandre Fillatre <afillatre@ippon.fr>
2016-09-05 08:42:19 +0200

    [JM Issue #570] Add missing retention on annotation

--- src/main/java/com/example/service/CustomService.java
+++ src/main/java/com/example/service/CustomService.java
@@ -2,10 +2,14 @@ package com.example.service;
 
 import org.springframework.stereotype.Service;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author afillatre@ippon.fr
  * @since 04/09/16.
  */
+@Retention(RetentionPolicy.RUNTIME)
 @Service
 public @interface CustomService {
 }
